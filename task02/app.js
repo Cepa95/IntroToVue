df079f4fb0c21e2ad9cb49c3623fa4e394c8666c
Cepa95 <ceprnic.josip@gmail.com>
2023-06-01 11:31:37 +0200

    task02

--- task02/app.js
+++ task02/app.js
@@ -1,10 +1,24 @@
 const app = Vue.createApp({
   data() {
     return {
-      courseGoal: "learning Vue",
+      courseGoalA: "learning Vue",
+    //   courseGoalB: "<h2>Mastering Vue<h2>",
+      courseGoalB: "Mastering Vue",
       vueLink: 'https://vuejs.org/'
     };
   },
+  methods:{
+    outputGoal(){
+        const randomNumber = Math.random();
+        if (randomNumber < 0.5){
+            return this.courseGoalA;
+        }
+        else{
+            return this.courseGoalB;
+        }
+    }
+  }
 });
+
 
 app.mount("#user-goal");
