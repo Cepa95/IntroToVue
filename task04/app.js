2086c09b07e7c5e008986e4a57f13bde3b48dec9
Cepa95 <ceprnic.josip@gmail.com>
2023-06-02 00:32:15 +0200

    second

--- task04/app.js
+++ task04/app.js
@@ -2,15 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: '',
     };
   },
   methods: {
-    add() {
-      this.counter++;
+    add(num) {
+      this.counter = this.counter + num;
     },
-    reduce() {
-      this.counter--;
+    reduce(num) {
+      this.counter = this.counter - num;
     },
+
+    setName(event){
+      this.name = event.target.value;
+    }
   },
 });
 
