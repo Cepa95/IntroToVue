6946a343e4f58833c3cde822f53bc04f3cfcc9c1
Cepa95 <ceprnic.josip@gmail.com>
2023-06-07 11:23:40 +0200

    task09

---
+++ part03/task09/app.js
@@ -0,0 +1,27 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            enteredValue: '',
+            tasks: [],
+            taskListIsVisible: true,
+        }
+    },
+    computed: {
+        buttonCaption(){
+            return this.taskListIsVisible ? 'Hide' : 'Show' 
+        }
+    },
+
+    methods: {
+        addTask() {
+            this.tasks.push(this.enteredValue)
+        },
+        toggleTaskList(){
+            this.taskListIsVisible = !this.taskListIsVisible
+        }
+    }
+
+
+});
+
+app.mount('#assignment')
