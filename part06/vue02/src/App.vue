135c3d2cff6ac5d6572c3335e456e92c4c173377
Cepa95 <ceprnic.josip@gmail.com>
2023-06-15 14:23:57 +0200

    App.vue

---
+++ part06/vue02/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<section>
+<h2> My friends</h2>
+<ul>
+    <li></li>
+</ul>
+</section>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "manuel lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          id: "julie",
+          name: "julie lorenz",
+          phone: "1111 1118 991",
+          email: "julie@localhost.com",
+        },
+      ],
+    };
+  },
+};
+</script>
