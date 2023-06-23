e837c9ad11a01d14f4008bf85bf71faa4afa09e5
Cepa95 <ceprnic.josip@gmail.com>
2023-06-24 00:03:49 +0200

    inject

--- part07/vue04/src/App.vue
+++ part07/vue04/src/App.vue
@@ -4,7 +4,7 @@
       :topic-title="activeTopic && activeTopic.title"
       :text="activeTopic && activeTopic.fullText"
     ></active-element>
-    <knowledge-base :topics="topics" @select-topic="activateTopic"></knowledge-base>
+    <knowledge-base @select-topic="activateTopic"></knowledge-base>
   </div>
 </template>
 
@@ -32,10 +32,23 @@ export default {
       activeTopic: null,
     };
   },
+  provide() {
+    return { topics: this.topics };
+  },
   methods: {
     activateTopic(topicId) {
       this.activeTopic = this.topics.find((topic) => topic.id === topicId);
     },
+  },
+  mounted() {
+    setTimeout(() => {
+      this.topics.push({
+        id: 'events',
+        title: 'Events',
+        description: 'checking events',
+        fullText: 'checking events full text',
+      });
+    }, 3000);
   },
 };
 </script>
