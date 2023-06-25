ee2a2cae3366d5e560f8d1f3e88857a23e2041cd
Cepa95 <ceprnic.josip@gmail.com>
2023-06-25 19:13:28 +0200

    global/local

--- part08/vue05/src/App.vue
+++ part08/vue05/src/App.vue
@@ -11,7 +11,18 @@
 </template>
 
 <script>
+import TheHeader from './components/TheHeader.vue';
+import BadgeList from './components/BadgeList.vue';
+import UserInfo from './components/UserInfo.vue';
+
 export default {
+  components: {
+    // 'the-header': TheHeader
+    // TheHeader: TheHeader
+    TheHeader,
+    BadgeList,
+    UserInfo,
+  },
   data() {
     return {
       activeUser: {
