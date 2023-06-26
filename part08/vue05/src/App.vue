5a496f10971b3dce3954d46fac46fc5ee44db8f1
Cepa95 <ceprnic.josip@gmail.com>
2023-06-26 15:08:58 +0200

    scoped slots

--- part08/vue05/src/App.vue
+++ part08/vue05/src/App.vue
@@ -7,14 +7,24 @@
       :info-text="activeUser.description"
       :role="activeUser.role"
     ></user-info>
+    <!-- <course-goals>
+      <template #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+      </template>
+    </course-goals> -->
+    <course-goals  #default="slotProps">
+        <h2>{{ slotProps.item }}</h2>
+        <p>{{ slotProps.anotherProp }}</p>
+    </course-goals>
   </div>
 </template>
 
 <script>
-import TheHeader from './components/TheHeader.vue';
-import BadgeList from './components/BadgeList.vue';
-import UserInfo from './components/UserInfo.vue';
-
+import TheHeader from "./components/TheHeader.vue";
+import BadgeList from "./components/BadgeList.vue";
+import UserInfo from "./components/UserInfo.vue";
+import CourseGoals from "./components/CourseGoals.vue";
 export default {
   components: {
     // 'the-header': TheHeader
@@ -22,13 +32,14 @@ export default {
     TheHeader,
     BadgeList,
     UserInfo,
+    CourseGoals,
   },
   data() {
     return {
       activeUser: {
-        name: 'Maximilian Schwarzmüller',
-        description: 'Site owner and admin',
-        role: 'admin',
+        name: "Maximilian Schwarzmüller",
+        description: "Site owner and admin",
+        role: "admin",
       },
     };
   },
