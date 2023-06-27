41748a87ed973a5c279f10bb1763d379d904cc37
Cepa95 <ceprnic.josip@gmail.com>
2023-06-27 20:48:43 +0200

    keep-alive

--- part08/vue05/src/App.vue
+++ part08/vue05/src/App.vue
@@ -21,7 +21,9 @@
     <button @click="setSelectedComponent('manage-goals')">Manage goals</button>
     <!-- <active-goals v-if="selectedComponent === 'active-goals'"></active-goals>
     <manage-goals v-if="selectedComponent === 'manage-goals' "></manage-goals> -->
+  <keep-alive>
   <component :is="selectedComponent"></component>
+  </keep-alive>
   </div>
 </template>
 
