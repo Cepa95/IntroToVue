c3737ba11818dc8bcf18e273e9709d3c1e77161a
Cepa95 <ceprnic.josip@gmail.com>
2023-06-28 18:17:06 +0200

    error alert

--- part08/vue05/src/components/ManageGoals.vue
+++ part08/vue05/src/components/ManageGoals.vue
@@ -1,6 +1,38 @@
 <template>
   <div>
     <h2>Manage goals</h2>
-    <input type="text" />
+    <input type="text" ref="goal" />
+    <button @click="setGoal">set goal</button>
+    <error-alert v-if="inputIsValid">
+      <h2>Input is invalid</h2>
+      <p>Please enter something</p>
+      <button @click="confirmError">Okay</button>
+    </error-alert>
   </div>
 </template>
+
+<script>
+import ErrorAlert from "./ErrorAlert.vue";
+export default {
+  components: {
+    ErrorAlert,
+  },
+  data() {
+    return {
+      inputIsValid: false,
+    };
+  },
+  methods: {
+    setGoal() {
+      const enteredValue = this.$refs.goal.value;
+      if (enteredValue === "") {
+        // alert('Input must not be empty')
+        this.inputIsValid = true;
+      }
+    },
+    confirmError() {
+      this.inputIsValid = false;
+    }
+  },
+};
+</script>
