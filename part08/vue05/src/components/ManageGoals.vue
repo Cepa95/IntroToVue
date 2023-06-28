729bf7bd43a8eb5e8d8f23d82faeac464ba5f0b1
Cepa95 <ceprnic.josip@gmail.com>
2023-06-28 18:34:59 +0200

    teleport

--- part08/vue05/src/components/ManageGoals.vue
+++ part08/vue05/src/components/ManageGoals.vue
@@ -3,11 +3,13 @@
     <h2>Manage goals</h2>
     <input type="text" ref="goal" />
     <button @click="setGoal">set goal</button>
+    <teleport to="body">
     <error-alert v-if="inputIsValid">
       <h2>Input is invalid</h2>
       <p>Please enter something</p>
       <button @click="confirmError">Okay</button>
     </error-alert>
+    </teleport>
   </div>
 </template>
 
