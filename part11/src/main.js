a45ba7b848bd79259bb0499e71722d51e7f501e9
Cepa95 <ceprnic.josip@gmail.com>
2023-07-20 22:10:57 +0200

    redirecting and catching routes

--- part11/src/main.js
+++ part11/src/main.js
@@ -5,13 +5,18 @@ import App from './App.vue';
 import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
+import NotFound from './components/nav/NotFound.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList },
+    { path: '/', redirect: '/teams' },
+    // { path: '/teams', component: TeamsList, alias: '/' }, // sa redirectom se minja url sa aliasom ne
+    { path: '/teams', component: TeamsList, alias: '/' },
     { path: '/users', component: UsersList },
     { path: '/teams/:teamId', component: TeamMembers, props: true },
+    // { path:'/:notFound(.*)', redirect: '/teams'}
+    { path:'/:NotFound(.*)', component: NotFound}
   ],
   linkActiveClass: 'active',
 });
