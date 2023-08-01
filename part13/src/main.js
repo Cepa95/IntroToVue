0d15d07392661a7855494d9e685b2d67ec7a8f88
Cepa95 <ceprnic.josip@gmail.com>
2023-08-01 20:33:36 +0200

    store

--- part13/src/main.js
+++ part13/src/main.js
@@ -1,87 +1,11 @@
 import { createApp } from 'vue';
-import { createStore } from 'vuex';
+
 
 import App from './App.vue';
-
-
-const counterModule = {
-  namespaced: true,
-  state() {
-    return {
-      counter: 0,
-
-    }
-  },
-  mutations: {
-    increment(state) {
-      state.counter++;
-    },
-    increase(state, payload) {
-      console.log(state)
-      state.counter = state.counter + payload.value;
-    },
-  },
-  actions: {
-    increment(context) {
-      setTimeout(function () {
-        context.commit('increment');
-      }, 2222);
-    },
-    increase(context, payload) {
-      console.log(context);
-      context.commit('increase', payload);
-    },
-  },
-  getters: {
-    finalCounter(state) {
-      return state.counter * 3;
-    },
-    normalizedCounter(_, getters) {
-      const finalCounter = getters.finalCounter;
-      if (finalCounter < 0) {
-        return 0;
-      } else if (finalCounter > 100) {
-        return 100;
-      }
-      return finalCounter;
-    },
-  },
-}
-
-const store = createStore({
-  modules: {
-    numbers: counterModule,
-  },
-  state() {
-    return {
-     
-      isLoggedIn: false,
-    };
-  },
-  mutations: {
-   
-    setAuth(state, payload) {
-      state.isLoggedIn = payload.isAuth;
-    },
-  },
-  actions: {
-   
-    login(context) {
-      context.commit('setAuth', { isAuth: true });
-    },
-    logout(context) {
-      context.commit('setAuth', { isAuth: false });
-    },
-  },
-  getters: {
-
-    userIsAuthenticated(state) {
-      return state.isLoggedIn;
-    },
-  },
-});
+import store from './store.js'
 
 const app = createApp(App);
+
 
 app.use(store);
 app.mount('#app');
