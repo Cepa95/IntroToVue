feee598e3638fa1a5fb05e1433613647d8ace2a1
Cepa95 <ceprnic.josip@gmail.com>
2023-08-29 09:57:15 +0200

    lifecycle hooks

--- part14/src/components/UserData.vue
+++ part14/src/components/UserData.vue
@@ -10,7 +10,16 @@
 
 
 <script>
-import { computed, inject } from 'vue';
+import {
+  computed,
+  inject,
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted
+} from 'vue';
 
 export default {
   props: ['firstName', 'lastName'],
@@ -19,10 +28,30 @@ export default {
       return props.firstName + ' ' + props.lastName;
     });
     // context.emit();
-    console.log(context)
-    const age = inject('userAge')
+    console.log(context);
+    const age = inject('userAge');
+    
+    onBeforeMount(function(){
+        console.log('onBeforeMount')
+    })
+    onMounted(function(){
+        console.log('onMounted')
+    })
+    onBeforeUpdate(function(){
+        console.log('onBeforeUpdate')
+    })
+    onUpdated(function(){
+        console.log('onUpdated')
+    })
+    onBeforeUnmount(function(){
+        console.log('onBeforeUnmount')
+    })
+    onUnmounted(function(){
+        console.log('onUnmounted')
+    })
+   
 
-    return { userName: uName, age: age};
+    return { userName: uName, age: age };
   },
 };
 </script>
